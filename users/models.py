68e1e169257defe4b4ce654dd5ce2cab3b4d6a5c
Shameem <shameemkhd@gmail.com>
2016-11-04 19:31:09 +0530

    Defined userinfo model and added to admin site.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    username = models.CharField(max_length=120)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
